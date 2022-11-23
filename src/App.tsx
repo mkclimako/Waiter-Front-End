e86c10bf43e4a6404c658666dfbe9b636556f380
markoclimako <mkclimako@gmail.com>
2022-11-23 16:10:29 -0300

    starting project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyles } from "./styles/GlobalStayles";
+import { Header } from "./components/Header";
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+  );
+}
+
+export default App;
