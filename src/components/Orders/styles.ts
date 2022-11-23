6a5bcee69afdc3cb8cfc1313954df8495214e6db
markoclimako <mkclimako@gmail.com>
2022-11-23 17:48:10 -0300

    feat(component/ordes): create orders component

---
+++ src/components/Orders/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: min(100%, 76rem);
+  margin: 2.5rem auto;
+
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+`;
+
